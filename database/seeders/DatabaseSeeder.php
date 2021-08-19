8753170c948617d1533aeb34b9000c4b623df2f1
cengiz <onkal.cengiz@gmail.com>
2021-08-19 21:58:33 +0300

    category model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +15,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(10)->create();
+        Category::factory(3)
+            ->create()
+            ->each(function ($category) {
+                Category::factory(3, ['category_id' => $category->id])
+                    ->create()->each(function ($category) {
+                        Category::factory(3, ['category_id' => $category->id])
+                            ->create();
+                    });
+            });
     }
 }
