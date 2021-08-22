35b9e37f99663388a41af1dd52c872c2dda6420a
cengiz <onkal.cengiz@gmail.com>
2021-08-22 13:00:19 +0300

    jetstream

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -11,6 +11,11 @@ class Category extends Model
 
     public function parent()
     {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
 
+    public function children()
+    {
+        return $this->hasMany(Category::class, 'category_id');
     }
 }
