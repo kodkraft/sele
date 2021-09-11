af971bcb6f4bcd679b799ffd745633d542719c83
cengiz <onkal.cengiz@gmail.com>
2021-09-11 22:36:21 +0300

    category controller

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('admin/categories', [\App\Http\Controllers\Admin\CategoryController::class, 'index'])->middleware('auth');
+
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
