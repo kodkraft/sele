254189e29775f36d85ad09bac9e39faa37efaa4d
cengiz <onkal.cengiz@gmail.com>
2021-09-21 23:04:00 +0300

    settings

---
+++ database/factories/Admin/SettingFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories\Admin;
+
+use App\Models\Admin\Setting;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class SettingFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Setting::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
