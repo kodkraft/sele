d6cf50b675001d494b4eb2e42794ef4e652bde44
cengiz <onkal.cengiz@gmail.com>
2021-09-23 22:23:06 +0300

    setting name

--- resources/views/admin/category-index.blade.php
+++ resources/views/admin/category-index.blade.php
@@ -1,7 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-    @livewire('category-management')
+    <div class="flex flex-row justify-end mt-8 container px-4">
+        <a href="{{action([\App\Http\Controllers\Admin\CategoryController::class,'create'])}}">
+            @lang('category.create')
+        </a>
+    </div>
+    <div class="flex flex-col mt-8">
+        <div class="flex flex-wrap">
+            @foreach($categories as $category)
+                <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+                    <a class="block relative h-48 rounded overflow-hidden">
+                        <img alt="ecommerce" class="object-cover object-center w-full h-full block"
+                             src="{{url($category->image()?->url)}}">
+                    </a>
+                    <div class="mt-4">
+                        <h2 class="text-gray-900 title-font text-lg font-medium">{{ $category->title }}</h2>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
 
 @push('js')
--- resources/views/livewire/category-management.blade.php
+++ resources/views/livewire/category-management.blade.php
@@ -1,23 +1,16 @@
 <div class="flex flex-col">
 
     <div class="flex flex-row justify-end mt-8 container px-4">
-        <button wire:click="createOrUpdate()"
-                class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-            @if($showForm)
-                @lang('category.show-categories')
-            @else
-                @lang('category.create')
-            @endif
-        </button>
+        <a href="">
+            @lang('category.show-categories')
+        </a>
     </div>
 
     <section class="text-gray-600">
         <div class="container mx-auto px-5 py-4">
-            @if($showForm)
-                @include('admin.components.category.create')
-            @else
-                @include('admin.components.category.index')
-            @endif
+
+            @include('admin.components.category.index')
+
         </div>
     </section>
 </div>
