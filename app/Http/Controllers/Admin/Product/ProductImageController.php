26ac0554e444cb2efaafbcdfa3a0b54e41effce0
cengiz <onkal.cengiz@gmail.com>
2021-09-28 00:45:29 +0300

    ProductController.php

---
+++ app/Http/Controllers/Admin/Product/ProductImageController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Product;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Image;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class ProductImageController extends Controller
+{
+    public function index(Product $product)
+    {
+        return view('admin/product/product-image-index')
+            ->with('product', $product);
+    }
+
+    public function store(Product $product, Request $request)
+    {
+        $fileName = $product->id . '_' . Str::random() . '.' . $request->file('image')->getClientOriginalExtension();
+        $path = 'products';
+        $request->file('image')->storeAs($path, $fileName, 'images');
+        $product->images()->create(['file_name' => $path . '/' . $fileName]);
+        return redirect()->back()->with('success', 'image created');
+    }
+
+    public function destroy(Product $category, Image $image)
+    {
+    }
+}
---
+++ resources/views/admin/product-index.blade.php
@@ -0,0 +1,33 @@
+<?php
+/**@var \App\Models\Product[] $products */
+
+?>
+@extends('layouts.app')
+@section('content')
+    <div class="flex flex-row justify-end mt-8 container px-4">
+        <a href="{{action([\App\Http\Controllers\Admin\ProductController::class,'create'])}}">
+            @lang('common.create')
+        </a>
+    </div>
+    <div class="flex flex-col mt-8">
+        <div class="flex flex-wrap">
+            @foreach($products as $product)
+                <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+                    <a class="block relative h-48 rounded overflow-hidden">
+                        <img alt="ecommerce" class="object-cover object-center w-full h-full block"
+                             src="{{url($product->image()?->url)}}">
+                    </a>
+                    <div class="mt-4">
+                        <h2 class="text-gray-900 title-font text-lg font-medium">{{ $product->title }}</h2>
+                        <a href="{{action([\App\Http\Controllers\Admin\Product\ProductImageController::class,'index'],['product'=>$product->id])}}">@lang('common.images')</a>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+@endsection
+
+@push('js')
+    @livewireScripts
+@endpush
+
