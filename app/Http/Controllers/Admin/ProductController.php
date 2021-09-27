c664a5d8150cd1376e87b2ca7f14c1ef074a56d4
cengiz <onkal.cengiz@gmail.com>
2021-09-28 00:35:34 +0300

    ProductController.php

---
+++ app/Http/Controllers/Admin/ProductController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+
+    public function index()
+    {
+        $products = Product::all();
+        return view('admin/product-index')
+            ->with('products', $products);
+    }
+
+
+    public function create()
+    {
+    }
+
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    public function show(Product $product)
+    {
+        //
+    }
+
+
+    public function edit(Product $product)
+    {
+        //
+    }
+
+
+    public function update(Request $request, Product $product)
+    {
+        //
+    }
+
+
+    public function destroy(Product $product)
+    {
+        //
+    }
+}
