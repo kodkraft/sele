56176e6aa86008893a6d7a0a59a61a84ae7735bd
cengiz <onkal.cengiz@gmail.com>
2021-12-02 23:28:17 +0200

    bootstrap product create

--- resources/views/admin/product-create.blade.php
+++ resources/views/admin/product-create.blade.php
@@ -13,6 +13,58 @@
             </a>
         </div>
 
+<div class="col-md-12 col-lg-6">
+    <form class="row g-3">
+        <div class="col-md-6">
+            <label for="title" class="form-label">@lang('common.title')</label>
+            <input type="text"
+                   id="title"
+                   name="title"
+                   class="form-control @error('title') is-invalid @enderror"
+                   value="{{old('title')}}">
+        </div>
+        <div class="col-md-6">
+            <label for="price" class="form-label">@lang('common.price')</label>
+            <input type="number" class="form-control" id="price" name="price">
+        </div>
+        <div class="col-12">
+            <label for="inputAddress" class="form-label">Address</label>
+            <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+        </div>
+        <div class="col-12">
+            <label for="inputAddress2" class="form-label">Address 2</label>
+            <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+        </div>
+        <div class="col-md-6">
+            <label for="inputCity" class="form-label">City</label>
+            <input type="text" class="form-control" id="inputCity">
+        </div>
+        <div class="col-md-4">
+            <label for="inputState" class="form-label">State</label>
+            <select id="inputState" class="form-select">
+                <option selected>Choose...</option>
+                <option>...</option>
+            </select>
+        </div>
+        <div class="col-md-2">
+            <label for="inputZip" class="form-label">Zip</label>
+            <input type="text" class="form-control" id="inputZip">
+        </div>
+        <div class="col-12">
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" id="gridCheck">
+                <label class="form-check-label" for="gridCheck">
+                    Check me out
+                </label>
+            </div>
+        </div>
+        <div class="col-12">
+            <button type="submit" class="btn btn-primary">Sign in</button>
+        </div>
+    </form>
+</div>
+
+
         <form action="{{action([\App\Http\Controllers\Admin\ProductController::class,'store'])}}"
               method="post">
             @csrf
