e31418cdd05c2f1c0b9e1e4c9109708f516d9912
cengiz <onkal.cengiz@gmail.com>
2021-12-16 22:54:36 +0200

    Home controller

--- routes/web.php
+++ routes/web.php
@@ -21,9 +21,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [\App\Http\Controllers\HomeController::class, 'index']);
 Route::get('/categories', 'App\Http\Controllers\Customer\CategoryController@index')->name('customer-category.index');
 
 Route::middleware(['auth'])->group(function () {
