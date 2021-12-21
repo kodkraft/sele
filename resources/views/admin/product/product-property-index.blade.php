0efecbcd81cf4dd0a68f0b3700ccb13bdbbb3fb3
cengiz <onkal.cengiz@gmail.com>
2021-12-22 00:36:30 +0200

    #12 ürün özellikleri ekleme

--- resources/views/admin/product/product-property-index.blade.php
+++ resources/views/admin/product/product-property-index.blade.php
@@ -6,38 +6,74 @@
 ?>
 @extends('layouts.app',['title'=>$product->title.' properties'])
 @section('content')
-    <form
-            action="{{action([\App\Http\Controllers\Admin\Product\ProductPropertyController::class,'store'],['product'=>$product->id])}}"
-            method="post">
-        @csrf
-        <select class="ml-2" name="property_id">
-            @foreach($properties as $prop)
-                <option value="{{$prop->id}}" @if(old('property_id')==$prop->id) selected @endif>{{$prop->name}}</option>
-            @endforeach
-        </select>
-        <input type="text" name="value" value="{{old('value')}}">
-        <button type="submit">Save</button>
-    </form>
-    <div class="flex flex-col mt-8">
-        <div class="flex flex-wrap">
-            @foreach($product->properties as $property)
 
-                {{$property->name}}=> {{$property->pivot->value}}
-                <div class="mt-4">
-                    <form action="{{action([\App\Http\Controllers\Admin\Product\ProductPropertyController::class,'destroy'],['product'=>$product->id,'property'=>$property->id])}}"
-                          method="post">
-                        @csrf
-                        @method('delete')
-                        <button type="submit"
-                                data-text="{{$property->name}} will be deleted."
-                                data-title="@lang('common.are_you_sure')"
-                                class="swal-submit">@lang('common.delete')</button>
-                    </form>
+    <h2>{{$product->title}} Properties</h2>
+    <hr>
+    <div class="row">
+
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            <form
+                    action="{{action([\App\Http\Controllers\Admin\Product\ProductPropertyController::class,'store'],['product'=>$product->id])}}"
+                    method="post"
+                    class="row g-3"
+            >
+                @csrf
+
+                <div class="col-md-12">
+                    <select class="form-control"
+                            id="property_id"
+                            name="property_id">
+                        @foreach($properties as $prop)
+                            <option value="{{$prop->id}}"
+                                    @if(old('property_id')==$prop->id) selected @endif>{{$prop->name}}</option>
+                        @endforeach
+                    </select>
                 </div>
 
-            @endforeach
+                <div class="col-md-12">
+                    <input type="text"
+                           class="form-control"
+                           name="value"
+                           placeholder="Value"
+                           value="{{old('value')}}">
+                </div>
+                <div class="col-12">
+                    <button type="submit" class="btn btn-primary">@lang('common.save')</button>
+                </div>
+            </form>
         </div>
     </div>
+    <div class="table-responsive">
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th>Property</th>
+                <th>Value</th>
+                <th>Action</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($product->properties as $prop)
+                <tr>
+                    <td>{{$prop->name}}</td>
+                    <td>{{$prop->value}}</td>
+                    <td>
+                        <form action="{{action([\App\Http\Controllers\Admin\Product\ProductPropertyController::class,'destroy'],['product'=>$product->id,'property'=>$prop->id])}}"
+                              method="post">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger">@lang('common.delete')</button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+
+
 @endsection
 
 
